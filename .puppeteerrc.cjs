8bd78444f0b5efe552bff0e133bba50affcaeb3c
Khd155 <Khaled.Kharmi@gmail.com>
2026-03-30 21:56:13 +0300

    Delete .puppeteerrc.cjs

--- .puppeteerrc.cjs
+++
@@ -1,9 +0,0 @@
-const { join } = require('path');
-
-/**
- * @type {import("puppeteer").Configuration}
- */
-module.exports = {
-  // إجبار التحميل داخل مجلد المشروع لتجنب مشاكل الصلاحيات في /root/
-  cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
-};
